88af02df7d559c6d9ba8edf55c2a40c59e63ede1
Bharath Balla <bharathballa3@gmail.com>
2021-06-12 20:42:58 +0530

    Added changes for Deploying to Tomcat

--- src/main/java/com/equafax/rmg/ResourcePlanningApplication.java
+++ src/main/java/com/equafax/rmg/ResourcePlanningApplication.java
@@ -2,9 +2,10 @@ package com.equafax.rmg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ResourcePlanningApplication {
+public class ResourcePlanningApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourcePlanningApplication.class, args);
